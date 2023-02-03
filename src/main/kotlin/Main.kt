9234fb7d97a926b47ccc83077d864feb406ffdc2
Deepak <pitaliya.d@gmail.com>
2023-02-03 11:57:39 +0530

    "Stage 6(final) completed"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,101 +1,107 @@
-// Initial state
-var water = 400
-var milk = 540
-var coffeeBeans = 120
-var cups = 9
-var money = 550
-var run = true  // Machine is running
-
 fun main() {
-    while(run) {
-        menu()
+    val coffeeMachine = CoffeeMachine()
+    while (coffeeMachine.run) {
+        coffeeMachine.menu()
     }
 }
 
-fun printStatus() {
-    val status = "The coffee machine has:\n" +
-            "$water ml of water\n" +
-            "$milk ml of milk\n" +
-            "$coffeeBeans g of coffee beans\n" +
-            "$cups disposable cups\n" +
-            "$$money of money"
-    println(status)
-}
+class CoffeeMachine {
 
-// Ask user what to do and take action accordingly
-fun menu() {
-    println("Write action (buy, fill, take, remaining, exit):")
-    when (readln()) {
-        "buy" -> {
-            println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
-            when (readln()) {
-                "1" -> {
-                    if (water >= 250 && coffeeBeans >= 16 && cups >= 1) {
-                        water -= 250
-                        coffeeBeans -= 16
-                        cups -= 1
-                        println("I have enough resources, making you a coffee!")
-                        money += 4
-                    } else {
-                        if (water < 250) println("Sorry, not enough water!")
-                        if (coffeeBeans < 16) println("Sorry, not enough coffee beans!")
-                        if (cups < 1) println("Sorry, not enough cups!")
+    private var water = 400
+    private var milk = 540
+    private var coffeeBeans = 120
+    private var cups = 9
+    private var money = 550
+    var run = true  // Machine is running
+
+    private fun takeInput(): String {
+        return readln()
+    }
+
+    private fun printStatus() {
+        val status = "The coffee machine has:\n" +
+                "$water ml of water\n" +
+                "$milk ml of milk\n" +
+                "$coffeeBeans g of coffee beans\n" +
+                "$cups disposable cups\n" +
+                "$$money of money"
+        println(status)
+    }
+
+    fun menu() {
+        println("Write action (buy, fill, take, remaining, exit):")
+        when (takeInput()) {
+            "buy" -> {
+                println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
+                when (takeInput()) {
+                    "1" -> {
+                        if (water >= 250 && coffeeBeans >= 16 && cups >= 1) {
+                            water -= 250
+                            coffeeBeans -= 16
+                            cups -= 1
+                            println("I have enough resources, making you a coffee!")
+                            money += 4
+                        } else {
+                            if (water < 250) println("Sorry, not enough water!")
+                            if (coffeeBeans < 16) println("Sorry, not enough coffee beans!")
+                            if (cups < 1) println("Sorry, not enough cups!")
+                        }
                     }
-                }
 
-                "2" -> {
-                    if (water >= 350 && milk >= 75 && coffeeBeans >= 20 && cups >= 1) {
-                        water -= 350
-                        milk -= 75
-                        coffeeBeans -= 20
-                        cups -= 1
-                        println("I have enough resources, making you a coffee!")
-                        money += 7
-                    } else {
-                        if (water < 350) println("Sorry, not enough water!")
-                        if (milk < 75) println("Sorry, not enough milk!")
-                        if (coffeeBeans < 20) println("Sorry, not enough coffee beans!")
-                        if (cups < 1) println("Sorry, not enough cups!")
+                    "2" -> {
+                        if (water >= 350 && milk >= 75 && coffeeBeans >= 20 && cups >= 1) {
+                            water -= 350
+                            milk -= 75
+                            coffeeBeans -= 20
+                            cups -= 1
+                            println("I have enough resources, making you a coffee!")
+                            money += 7
+                        } else {
+                            if (water < 350) println("Sorry, not enough water!")
+                            if (milk < 75) println("Sorry, not enough milk!")
+                            if (coffeeBeans < 20) println("Sorry, not enough coffee beans!")
+                            if (cups < 1) println("Sorry, not enough cups!")
+                        }
                     }
-                }
 
-                "3" -> {
-                    if (water >= 200 && milk >= 100 && coffeeBeans >= 12 && cups >= 1) {
-                        water -= 200
-                        milk -= 100
-                        coffeeBeans -= 12
-                        cups -= 1
-                        println("I have enough resources, making you a coffee!")
-                        money += 6
-                    } else {
-                        if (water < 200) println("Sorry, not enough water!")
-                        if (milk < 100) println("Sorry, not enough milk!")
-                        if (coffeeBeans < 12) println("Sorry, not enough coffee beans!")
-                        if (cups < 1) println("Sorry, not enough cups!")
+                    "3" -> {
+                        if (water >= 200 && milk >= 100 && coffeeBeans >= 12 && cups >= 1) {
+                            water -= 200
+                            milk -= 100
+                            coffeeBeans -= 12
+                            cups -= 1
+                            println("I have enough resources, making you a coffee!")
+                            money += 6
+                        } else {
+                            if (water < 200) println("Sorry, not enough water!")
+                            if (milk < 100) println("Sorry, not enough milk!")
+                            if (coffeeBeans < 12) println("Sorry, not enough coffee beans!")
+                            if (cups < 1) println("Sorry, not enough cups!")
+                        }
                     }
-                }
 
-                else -> menu()
+                    else -> menu()
+                }
             }
-        }
 
-        "fill" -> {
-            println("Write how many ml of water you want to add:")
-            water += readln().toInt()
-            println("Write how many ml of milk you want to add:")
-            milk += readln().toInt()
-            println("Write how many grams of coffee beans you want to add:")
-            coffeeBeans += readln().toInt()
-            println("Write how many disposable cups you want to add: ")
-            cups += readln().toInt()
-        }
+            "fill" -> {
+                println("Write how many ml of water you want to add:")
+                water += takeInput().toInt()
+                println("Write how many ml of milk you want to add:")
+                milk += takeInput().toInt()
+                println("Write how many grams of coffee beans you want to add:")
+                coffeeBeans += takeInput().toInt()
+                println("Write how many disposable cups you want to add: ")
+                cups += takeInput().toInt()
+            }
 
-        "take" -> {
-            println("I gave you $$money")
-            money = 0
-        }
+            "take" -> {
+                println("I gave you $$money")
+                money = 0
+            }
 
-        "remaining" -> printStatus()
-        "exit" -> run = false
+            "remaining" -> printStatus()
+            "exit" -> run = false
+        }
     }
 }
